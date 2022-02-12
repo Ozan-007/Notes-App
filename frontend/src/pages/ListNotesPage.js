7e3387f7f7620701ef96499f5e01c7acc8e5fbd7
Ozan <ozan28061@gmail.com>
2022-02-12 14:40:20 +0100

    api fetched Cors error occured in ListNotesPage.js

--- frontend/src/pages/ListNotesPage.js
+++ frontend/src/pages/ListNotesPage.js
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, { useState,useEffect } from 'react'
 
 const ListNotesPage = () => {
-  return (
-
-    <div>NotesPage</div>
+  
+  let [notes, setNotes] = useState([])
+  
+  useEffect(() => {
+    getNotes()
+    }, [])
     
-  )
+  const getNotes = async () => {
+    let response = await fetch("http://localhost:8000/api/notes/")
+    let data = await response.json()
+    console.log(notes)
+    setNotes(data)
+  }
+
+  return (
+    <div>NOTES</div>
+    )
+
 }
 
 export default ListNotesPage
