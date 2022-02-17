929456446f06ec8132b93aefbb22fc5109523bd0
Ozan <ozan28061@gmail.com>
2022-02-17 19:09:46 +0100

    getNote async func created api fetched and used

--- frontend/src/pages/NotePage.js
+++ frontend/src/pages/NotePage.js
@@ -1,8 +1,30 @@
 import React from 'react'
+import {useState,useEffect} from 'react'
+import { useParams } from 'react-router-dom'
 
 const NotePage = () => {
+
+  let noteId = useParams()
+
+  let [note, setNote] = useState(null)
+
+
+  useEffect(() => {
+    getNote()
+  })
+  
+  let getNote = async () => {
+    let response = await fetch(`/api/notes/${noteId.id}`)
+    let data = await response.json()
+    setNote(data)
+  }
+  
+  
+
   return (
-    <div>NotePage</div>
+    <div>
+      <p>{note?.body}</p>
+    </div>
   )
 }
 
